2194eb6e208a136308f3b371a636a3ea9ae9222c
unknown <afyaakhudur@gmail.com>
2024-02-22 15:55:53 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 9 ;
+let num2 = 7 ;
+let multiply = (num1 * num2) - 1
+console.log(multiply)
+
+let random = Math.ceil(Math.random()) 
+console.log(random);
+
+let num4 = 5 ;
+let num3 = 19;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(6 , 7 ,20)
+console.log(max);
